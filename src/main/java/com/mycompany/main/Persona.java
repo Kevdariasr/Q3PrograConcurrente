3a0f739dcf87e4e19af7d75a33d19d981bdf05cc
Kevin Arias <kevdariasr@gmail.com>
2022-09-27 11:52:21 -0600

    Prueba 1

---
+++ src/main/java/com/mycompany/main/Persona.java
@@ -0,0 +1,22 @@
+package com.mycompany.main;
+
+/**
+ *
+ * @author kevda
+ */
+public class Persona {
+    
+  private String nombre;
+public Persona(){
+    
+}  
+
+   public String getNombre(){
+       return nombre;
+   } 
+    
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+    
+}
